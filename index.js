2b24891beacb72e6cf367f33882cca3ddbf97fbf
RiCK BROWN <ricky.brown.00@gmail.com>
2018-06-17 00:27:49 +0100

    Enabled cookies

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
 const keys = require('./config/keys')
 
 require('./Models/User')
 require('./Services/passport')
 
+// Mongoose connection inc error handling
 mongoose.connect(keys.mongoURI)
   .then(() => console.log('connected to mongo')) 
-  .catch(error => console.error('App starting error:', error.stack)) 
+  .catch(error => console.error('App starting error: ', error.stack)) 
 
+// Global variables
 const app = express()
+const thirtyDays = 30*24*60*60*1000
+
+// Middleware
+app.use(
+  cookieSession({
+    maxAge: thirtyDays,
+    keys: [keys.cookieKey]
+  })
+)
+
+app.use(passport.initialize())
+
+app.use(passport.session())
 
 require('./Routes/authRoutes')(app)
 
+// Web Server connection
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
