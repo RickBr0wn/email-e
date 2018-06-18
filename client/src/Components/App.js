c40eb1165666f66ab7224c03aa6efa2488fde00f
RiCK BROWN <ricky.brown.00@gmail.com>
2018-06-18 16:48:54 +0100

    Still bugged

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,36 +1,32 @@
 import React, { Component } from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
 import { connect } from 'react-redux'
+import * as actions from '../Actions'
 
-class Header extends Component {
-  renderContent() {
-    switch (this.props.auth) {
-      case null:
-        return
-      case false:
-        return <li><a href="/auth/google">Login With Google</a></li>
-      default:
-        return <li><a>Logout</a></li>
-    }
+import Header from './Header'
+const Dashboard = () => <div className="container"><h2>Dashboard</h2></div>
+const SurveyNew = () => <div className="container"><h2>SurveyNew</h2></div>
+const Landing = () => <div className="container"><h2>Landing</h2></div>
+
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser()
   }
 
   render() {
     return (
-      <nav>
-        <div className="nav-wrapper">
-          <a className="left brand-logo">
-            Emaily
-          </a>
-          <ul className="right">
-            {this.renderContent()}
-          </ul>
-        </div>
-      </nav>
+      <div>
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
     )
   }
 }
 
-function mapStateToProps({ auth }) {
-  return { auth }
-}
-
-export default connect(mapStateToProps)(Header)
+export default connect(null, actions)(App)
--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -2,24 +2,32 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return
+      case false:
+        return <li><a href="/auth/google">Login With Google</a></li>
+      default:
+        return <li><a>Logout</a></li>
+    }
+  }
+
   render() {
-    console.log(this.props)
     return (
       <nav>
         <div className="nav-wrapper">
-          <a className="left brand-logo">eMail-e</a>
+          <a className="left brand-logo">
+            Emaily
+          </a>
           <ul className="right">
-            <li>
-              <a>Login with Google</a>
-            </li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
     )
   }
 }
-
-// mapStateToProps = { auth } => { auth }
 
 function mapStateToProps({ auth }) {
   return { auth }
