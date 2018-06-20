bf6565e0c3f8ef81596a46cc3c30bbd3125491a2
RiCK BROWN <ricky.brown.00@gmail.com>
2018-06-20 23:47:53 +0100

    Added survey

--- Routes/surveyRoutes.js
+++ Routes/surveyRoutes.js
@@ -1,7 +1,23 @@
+const mongoose = require('mongoose')
 const requireLogin = require('../Middlewares/requireLogin')
+const requireCredits = require('../Middlewares/requireCredits')
+
+const Survey = mongoose.model('surveys')
 
 module.exports = app => {
   app.post('/api/surveys', requireLogin, (req, res) => {
-    
+    const { title, subject, body, recipients } = req.body
+
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+      _user: req.user.id,
+      dateSent: Date.now()
+    })
+
+    survey.save()
+
   })
 }
